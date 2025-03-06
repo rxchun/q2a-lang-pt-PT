d400d639b05bd2c8d4ec3ce7879430ff4053ef04
Chun <12265739+rxchun@users.noreply.github.com>
2025-03-06 23:56:01 +0000

    Initial commit

---
+++ formal/pt-PT/qa-lang-profile.php
@@ -0,0 +1,90 @@
+<?php
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/
+
+	Description: Language phrases for user profile page
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+return array(
+	'1_chosen_as_best' => ' (1 melhor resposta)',
+	'1_down_vote' => '1 voto negativo',
+	'1_up_vote' => '1 voto positivo',
+	'1_with_best_chosen' => ' (aceitou 1 resposta)',
+	'activity_by_x' => 'Atividade do(a) ^',
+	'answers' => 'Respostas:',
+	'answers_by_x' => 'Respostas do(a) ^',
+	'bonus_points' => 'Bónus de pontos:',
+	'comments' => 'Comentários:',
+	'delete_pm_popup' => 'Apagar esta mensagem privada',
+	'delete_wall_post_popup' => 'Apagar esta publicação no mural',
+	'extra_privileges' => 'Privilégios adicionais:',
+	'gave_out' => 'Deu:',
+	'my_account_title' => 'A minha conta',
+	'no_answers_by_x' => 'Nenhuma resposta do(a) ^',
+	'no_posts_by_x' => 'Nenhuma publicação do(a) ^',
+	'no_questions_by_x' => 'Nenhuma pergunta do(a) ^',
+	'permit_anon_view_ips' => 'Ver IPs de publicações anónimas',
+	'permit_close_q' => 'Fechar qualquer pergunta',
+	'permit_delete_hidden' => 'Apagar publicações ocultas',
+	'permit_edit_a' => 'Editar qualquer resposta',
+	'permit_edit_c' => 'Editar qualquer comentário',
+	'permit_edit_q' => 'Editar qualquer pergunta',
+	'permit_edit_silent' => 'Editar publicações silenciosamente',
+	'permit_flag' => 'Denunciar publicações',
+	'permit_hide_show' => 'Ocultar ou mostrar qualquer publicação',
+	'permit_moderate' => 'Aprovar ou rejeitar publicações',
+	'permit_post_a' => 'Responder perguntas',
+	'permit_post_c' => 'Adicionar comentários',
+	'permit_post_q' => 'Fazer perguntas',
+	'permit_post_wall' => 'Publicar em murais de usuários',
+	'permit_recat' => 'Recategorizar qualquer pergunta',
+	'permit_retag' => 'Modificar tags de qualquer pergunta',
+	'permit_select_a' => 'Selecionar uma resposta de qualquer pergunta',
+	'permit_view_q_page' => 'Ver páginas de perguntas',
+	'permit_view_new_users_page' => 'Ver página de usuários mais recentes',
+	'permit_view_special_users_page' => 'Ver página de usuários especiais',
+	'permit_view_voters_flaggers' => 'Ver quem votou ou denunciou publicações',
+	'permit_vote_a' => 'Votar em respostas',
+	'permit_vote_c' => 'Votar em comentários',
+	'permit_vote_down' => 'Votar negativamente em publicações',
+	'permit_vote_q' => 'Votar em perguntas',
+	'post_wall_blocked' => 'Este usuário desativou publicações no seu mural',
+	'post_wall_button' => 'Adicionar ao mural',
+	'post_wall_empty' => 'Por favor escreva algo para publicar neste mural',
+	'post_wall_limit' => 'De momento não pode escrever mais publicações de mural',
+	'post_wall_must_be_approved' => 'A sua conta deve primeiro ser aprovada para poder publicar neste mural. Por favor aguarde ou ^1adicione mais informações ao seu perfil^2.',
+	'post_wall_must_confirm' => 'Por favor ^5confirme o seu endereço de e-mail^6 para poder publicar neste mural.',
+	'post_wall_must_login' => 'Por favor, ^1inicie sessão^2 ou ^3registe-se^4 para publicar neste mural.',
+	'questions' => 'Perguntas:',
+	'questions_by_x' => 'Perguntas do(a) ^',
+	'ranked_x' => ' (classificado #^)',
+	'received' => 'Recebeu:',
+	'recent_activity_by_x' => 'Atividade recente do(a) ^',
+	'score' => 'Pontuação:',
+	'send_private_message' => ' - ^1enviar mensagem privada^2',
+	'set_bonus_button' => 'Atualizar bónus',
+	'title' => 'Título:',
+	'user_x' => 'Usuário ^',
+	'user_x_disabled_pms' => '^ desativou mensagens privadas.',
+	'voted_on' => 'Votou em:',
+	'wall_for_x' => 'Mural do(a) ^',
+	'wall_view_more' => 'Ver mais publicações de Mural...',
+	'x_chosen_as_best' => ' (^ melhor respostas)',
+	'x_down_votes' => '^ votos negativos',
+	'x_up_votes' => '^ votos positivos',
+	'x_with_best_chosen' => ' (aceitou ^ respostas)',
+);
---
+++ informal/pt-PT/qa-lang-emails.php
@@ -0,0 +1,85 @@
+<?php
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/
+
+	Description: Language phrases for email notifications
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+return array(
+	'a_commented_body' => "A tua resposta em ^site_title tem um novo comentário de ^c_handle:\n\n^open^c_content^close\n\nA tua resposta foi:\n\n^open^c_context^close\n\nPodes responder adicionando o teu próprio comentário:\n\n^url\n\nObrigado,\n\n^site_title",
+	'a_commented_subject' => 'A tua resposta em ^site_title tem um novo comentário',
+
+	'a_followed_body' => "A tua resposta em ^site_title tem uma nova pergunta relacionada por ^q_handle:\n\n^open^q_title^close\n\nA tua resposta foi:\n\n^open^a_content^close\n\nClica abaixo para responder à nova pergunta:\n\n^url\n\nObrigado,\n\n^site_title",
+	'a_followed_subject' => 'A tua resposta ^site_title tem uma pergunta relacionada',
+
+	'a_selected_body' => "Parabéns! A tua resposta em ^site_title foi selecionada como melhor resposta por ^s_handle:\n\n^open^a_content^close\n\nA pergunta era:\n\n^open^q_title^close\n\nClica abaixo para ver a tua resposta:\n\n^url\n\nObrigado,\n\n^site_title",
+	'a_selected_subject' => 'A tua resposta em ^site_title foi selecionada!',
+
+	'c_commented_body' => "Um novo comentário de ^c_handle foi adicionado após o teu comentário em ^site_title:\n\n^open^c_content^close\n\nA discussão está a seguir:\n\n^open^c_context^close\n\nPodes responder adicionando outro comentário:\n\n^url\n\nObrigado,\n\n^site_title",
+	'c_commented_subject' => 'O teu comentário em ^site_title foi adicionado a',
+
+	'confirm_body' => "Por favor clica no link abaixo para confirmar o teu e-mail para ^site_title.\n\n^url\n\nCódigo de confirmação: ^code\n\n Obrigado,\n^site_title",
+	'confirm_subject' => '^site_title - Confirmação de e-mail',
+
+	'feedback_body' => "Comentários:\n^message\n\nNome:\n^name\n\nE-mail:\n^email\n\nPágina anterior:\n^previous\n\nUsuário:\n^url\n\nEndereço IP:\n^ip\n\nNavegador:\n^browser",
+	'feedback_subject' => '^ feedback',
+
+	'flagged_body' => "Uma publicação de ^p_handle recebeu ^flags:\n\n^open^p_context^close\n\nClica abaixo para ver a publicação:\n\n^url\n\n\nClica abaixo para rever todas as publicações denunciadas:\n\n^a_url\n\n\nObrigado,\n\n^site_title",
+	'flagged_subject' => '^site_title tem uma publicação denunciada',
+
+	'moderate_body' => "Uma publicação de ^p_handle requer a tua aprovação:\n\n^open^p_context^close\n\nClica abaixo para aprovar ou rejeitar a publicação:\n\n^url\n\n\nClica abaixo para rever todas as publicações na fila:\n\n^a_url\n\n\nObrigado,\n\n^site_title",
+	'moderate_subject' => 'Moderação em ^site_title',
+
+	'new_password_body' => "A tua nova palavra-passe para ^site_title encontra-se abaixo. \n\nPassword: ^password\n\nÉ recomendável alterar esta palavra-passe imediatamente após iniciar sessão. \n\nObrigado,\n^site_title\n^url",
+	'new_password_subject' => '^site_title - A tua nova palavra-passe',
+
+	'private_message_body' => "Recebes-te uma mensagem privada de ^f_handle em ^site_title:\n\n^abrir^mensagem^fechar\n\n^moreObrigado,\n\n^site_title\n\n\nPara bloquear mensagens privadas, visita o perfil da tua conta:\n^a_url",
+	'private_message_info' => "Mais informações sobre ^f_handle:\n\n^url\n\n",
+	'private_message_reply' => "Clica abaixo para responderes a ^f_handle em mensagem privada:\n\n^url\n\n",
+	'private_message_subject' => 'Mensagem de ^f_handle em ^site_title',
+
+	'q_answered_body' => "A tua pergunta em ^site_title foi respondida por ^a_handle:\n\n^open^a_content^close\n\nA tua pergunta foi:\n\n^open^q_title^close\n\nSe gostas-te desta resposta, podes seleccioná-la como melhor resposta:\n\n^url\n\nObrigado,\n\n^site_title",
+	'q_answered_subject' => 'A tua pergunta em ^site_title foi respondida',
+
+	'q_commented_body' => "A tua pergunta em ^site_title tem um novo comentário de ^c_handle:\n\n^open^c_content^close\n\nA tua pergunta foi:\n\n^open^c_context^close\n\nPodes responder adicionando o teu próprio comentário:\n\n^url\n\nObrigado,\n\n^site_title",
+	'q_commented_subject' => 'A tua pergunta em ^site_title tem um novo comentário',
+
+	'q_posted_body' => "Uma nova pergunta foi feita por ^q_handle:\n\n^open^q_title\n\n^q_content^close\n\nClica abaixo para ver a pergunta:\n\n^url\n\nObrigado,\n\n^site_title",
+	'q_posted_subject' => '^site_title tem uma nova pergunta',
+
+	'remoderate_body' => "Uma publicação editada por ^p_handle requer a tua reaprovação:\n\n^open^p_context^close\n\nClica abaixo para aprovar ou ocultar a publicação editada:\n\n^url\n\n\nClica abaixo para rever todas as publicações na fila:\n\n^a_url\n\n\nObrigado,\n\n^site_title",
+	'remoderate_subject' => 'Moderação - Uma publicação editada em ^site_title requer reaprovação',
+
+	'reset_body' => "Clica abaixo para redefinir a tua palavra-passe para ^site_title. \n\n^url\n\nEm alternativa, introduz o código no campo abaixo fornecido. \n\nCódigo: ^code\n\nSe não solicitou a redefinição da tua palavra-passe, ignore esta mensagem. \n\nObrigado,\n^site_title",
+	'reset_subject' => '^site_title - Redefinir palavra-passe esquecida',
+
+	'to_handle_prefix' => "^,\n\n",
+
+	'u_registered_body' => "Um novo utilizador foi registado como ^u_handle. \n\nClica abaixo para ver o perfil do utilizador:\n\n^url\n\nObrigado,\n\n^site_title",
+	'u_registered_subject' => '^site_title tem um novo utilizador registado',
+	'u_to_approve_body' => "Um novo utilizador registou-se como ^u_handle. \n\nClica abaixo para aprovar o utilizador:\n\n^url\n\nClica abaixo para analisar todos os utilizadores que aguardam aprovação:\n\n^a_url\n\nObrigado,\n\n^site_title",
+
+	'u_approved_body' => "Podes ver o teu novo perfil de utilizador aqui:\n\n^url\n\nObrigado,\n\n^site_title",
+	'u_approved_subject' => 'A tua conta em ^site_title foi aprovada',
+
+	'wall_post_body' => "^f_handle publicou no mural do teu perfil em ^site_title:\n\n^open^post^close\n\nPodes responder à publicação aqui:\n\n^url\n\nObrigado,\n\n^site_title",
+	'wall_post_subject' => 'Nova pulicação no teu mural em ^site_title',
+
+	'welcome_body' => "Obrigado por te registares no ^site_title. \n\n^custom^confirm Os teus dados para início de sessão são os seguintes:\n\nNome de utilizador: ^handle\nE-mail: ^email\n\nMantem estas informações seguras para futura referência. \n\nObrigado,\n\n^site_title\n^url",
+	'welcome_confirm' => "Por favor clica no link abaixo para confirmar o teu endereço de e-mail.\n\n^url\n\n",
+	'welcome_subject' => 'Bem-vindo(a) ao ^site_title!',
+);
